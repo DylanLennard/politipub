4d2f6858c75775756a3cacbd1a4887b0bbe406f6
drownedout <danielcdonnelly@gmail.com>
2016-02-17 14:48:20 -0500

    Edits to Article controller (new, show), Article model added, view edits

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,46 @@
 class ArticlesController < ApplicationController
+
+	
+	def index
+		@articles = Article.all
+	end
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(article_params)
+		if @article.save
+			redirect_to @article
+		else
+			render 'new'
+		end
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+
+	end
+
+	def search
+	end
+
+	def destroy
+	end
+
+	private
+
+	def article_params
+		params.require(:article).permit(:title, :author, :body)
+	end
+
+	def find_article
+		@article = Article.find(params[:id])
+	end
 end
