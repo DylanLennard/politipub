19d35cfcdedb8fc25b0bfa79ff70c4cb0e36488f
drownedout <danielcdonnelly@gmail.com>
2016-03-17 18:30:13 -0400

    Updating S3 views

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ActiveRecord::Base
 	searchkick
 	paginates_per 8
-	has_attached_file :banner_image, styles: { medium: "300x300#", small: "100x100#" }
+	has_attached_file :banner_image, styles: { large: "900x900", medium: "300x300#", small: "100x100#" }
   	validates_attachment_content_type :banner_image, :content_type => /\Aimage\/.*\Z/
 end
