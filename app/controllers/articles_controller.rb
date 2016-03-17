239afb64eaea147c6e16b5d14549d01721cec599
drownedout <danielcdonnelly@gmail.com>
2016-03-17 19:42:30 -0400

    Style Changes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,7 +9,7 @@ class ArticlesController < ApplicationController
 	end
 
 	def index
-		@articles = Article.all.order(:id).page params[:page]
+		@articles = Article.all.order("created_at DESC").page params[:page]
 	end
 
 	def new
