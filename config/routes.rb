e22a39586bcd732785757aec4740a51b8bb998d3
drownedout <danielcdonnelly@gmail.com>
2016-04-11 22:21:32 -0400

    Devise edits

--- config/routes.rb
+++ config/routes.rb
@@ -10,10 +10,10 @@ Rails.application.routes.draw do
 
   resources :authors
 
-  get 'pages/about'
-  get 'pages/contact'
-  get 'pages/sponsor'
-  get 'pages/ontap'
-  get 'pages/dashboard'
+  get 'about', :to => 'pages#about', :as => :pages_about
+  get 'contact', :to => 'pages#contact', :as => :pages_contact
+  get 'sponsor', :to => 'pages#sponsor', :as => :pages_sponsor
+  get 'ontap', :to => 'pages#ontap', :as => :pages_ontap
+  get 'dashboard', :to => 'pages#dashboard', :as => :pages_dashboard
 
 end
