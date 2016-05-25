2a5364c28626c5e5521b48491d7a2212d5599ad6
drownedout <danielcdonnelly@gmail.com>
2016-05-25 19:34:15 -0400

    Article Tests

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
+include Devise::TestHelpers
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	def setup
+		@request.env["devise.mapping"] = Devise.mappings[:admin]
+		@article = Article.new(title: "Article Title")
+	end
+
+	test "Article should be valid" do 
+		assert @article.valid?
+	end
+
 end
