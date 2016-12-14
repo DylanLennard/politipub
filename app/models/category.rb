dce7c2b8c783d878930dbe31a46bdbb5280d7872
drownedout <danielcdonnelly@gmail.com>
2016-12-13 21:04:10 -0500

    Added images and description to categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,6 @@ class Category < ActiveRecord::Base
 	validates_uniqueness_of :name
 	has_many :article_categories
 	has_many :articles, through: :article_categories
+	has_attached_file :image, styles: { large: "900x900", medium: "525x320#", small: "225x150#" }
+  	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161208010920) do
+ActiveRecord::Schema.define(version: 20161214015700) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
@@ -84,8 +84,13 @@ ActiveRecord::Schema.define(version: 20161208010920) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+    t.text     "description"
   end
 
   create_table "ckeditor_assets", force: :cascade do |t|
