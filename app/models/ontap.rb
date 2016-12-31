2234bd549c51b3962923f18080bead0ec7cd9c40
drownedout <danielcdonnelly@gmail.com>
2016-12-31 16:47:30 -0500

    Bug fixes

--- app/models/ontap.rb
+++ app/models/ontap.rb
@@ -1,6 +1,7 @@
 class Ontap < ActiveRecord::Base
 	belongs_to :admin
 	belongs_to :author
+	validates_presence_of :banner
 	searchkick
 	paginates_per 10
 	has_attached_file :banner, styles: { large: "900x900", medium: "525x320#", small: "225x150#" }
